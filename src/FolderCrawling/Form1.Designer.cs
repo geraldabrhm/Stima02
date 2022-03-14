2dfb39fcd37f83f2d219e4732627498ee3475bc0
geraldabrhm <geraldabraham16@gmail.com>
2022-03-14 20:53:48 +0700

    Adding item implementation, initial DFS BFS, and change .csproj

--- src/FolderCrawling/Form1.Designer.cs
+++ src/FolderCrawling/Form1.Designer.cs
@@ -50,7 +50,8 @@ namespace FolderCrawling
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(68, 99);
+            this.label1.Location = new System.Drawing.Point(45, 64);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 0;
@@ -60,18 +61,20 @@ namespace FolderCrawling
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label2.Location = new System.Drawing.Point(13, 67);
+            this.label2.Location = new System.Drawing.Point(9, 44);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1250, 2);
+            this.label2.Size = new System.Drawing.Size(833, 1);
             this.label2.TabIndex = 1;
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.label3.Location = new System.Drawing.Point(416, 67);
+            this.label3.Location = new System.Drawing.Point(277, 44);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(2, 500);
+            this.label3.Size = new System.Drawing.Size(1, 325);
             this.label3.TabIndex = 2;
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -80,7 +83,8 @@ namespace FolderCrawling
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(68, 132);
+            this.label4.Location = new System.Drawing.Point(45, 86);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(190, 18);
             this.label4.TabIndex = 3;
@@ -90,9 +94,10 @@ namespace FolderCrawling
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(72, 167);
+            this.button1.Location = new System.Drawing.Point(48, 109);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 30);
+            this.button1.Size = new System.Drawing.Size(104, 19);
             this.button1.TabIndex = 4;
             this.button1.Text = "Choose Folder...";
             this.button1.UseVisualStyleBackColor = true;
@@ -102,17 +107,20 @@ namespace FolderCrawling
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(248, 174);
+            this.label5.Location = new System.Drawing.Point(165, 113);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(100, 16);
             this.label5.TabIndex = 5;
             this.label5.Text = "No File Chosen";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(68, 214);
+            this.label6.Location = new System.Drawing.Point(45, 139);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(110, 18);
             this.label6.TabIndex = 6;
@@ -121,28 +129,33 @@ namespace FolderCrawling
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(72, 252);
+            this.textBox1.Location = new System.Drawing.Point(48, 164);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(314, 22);
+            this.textBox1.Size = new System.Drawing.Size(211, 22);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "e.g. \"IF2200.pdf\"";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.Location = new System.Drawing.Point(72, 306);
+            this.checkBox1.Location = new System.Drawing.Point(48, 199);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(136, 20);
             this.checkBox1.TabIndex = 8;
             this.checkBox1.Text = "Find all occurence";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(452, 99);
+            this.label7.Location = new System.Drawing.Point(301, 64);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(64, 20);
             this.label7.TabIndex = 10;
@@ -152,7 +165,8 @@ namespace FolderCrawling
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(546, 20);
+            this.label8.Location = new System.Drawing.Point(364, 13);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(200, 29);
             this.label8.TabIndex = 11;
@@ -162,7 +176,8 @@ namespace FolderCrawling
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton1.Location = new System.Drawing.Point(72, 388);
+            this.radioButton1.Location = new System.Drawing.Point(48, 252);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(52, 20);
             this.radioButton1.TabIndex = 12;
@@ -175,7 +190,8 @@ namespace FolderCrawling
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(68, 348);
+            this.label9.Location = new System.Drawing.Point(45, 226);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(188, 18);
             this.label9.TabIndex = 13;
@@ -186,28 +202,32 @@ namespace FolderCrawling
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton2.Location = new System.Drawing.Point(72, 418);
+            this.radioButton2.Location = new System.Drawing.Point(48, 272);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(53, 20);
             this.radioButton2.TabIndex = 14;
             this.radioButton2.TabStop = true;
             this.radioButton2.Text = "DFS";
             this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(177, 467);
+            this.button2.Location = new System.Drawing.Point(118, 304);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 29);
+            this.button2.Size = new System.Drawing.Size(69, 19);
             this.button2.TabIndex = 15;
             this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1280, 585);
+            this.ClientSize = new System.Drawing.Size(853, 380);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.label9);
@@ -223,7 +243,6 @@ namespace FolderCrawling
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.InputForm_Load);
--- src/FolderCrawling/Form1.cs
+++ src/FolderCrawling/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace FolderCrawling
 {
@@ -49,7 +50,90 @@ namespace FolderCrawling
 
         private void button1_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("c:\\");
+            FolderBrowserDialog objPath = new FolderBrowserDialog();
+            string selectedDir = string.Empty;
+
+            if (objPath.ShowDialog() == DialogResult.OK)
+            {
+                selectedDir = objPath.SelectedPath;
+                MessageBox.Show("You have selected " + selectedDir);
+                label5.Text = selectedDir;
+                label5.ForeColor = System.Drawing.Color.Green;
+
+                /* This is how to get file and subdirectory names of selectedDir */
+                //string[] dirs = Directory.GetDirectories(selectedDir, "*.", SearchOption.TopDirectoryOnly);
+                //string[] files = Directory.GetFiles(selectedDir);
+                /* Display the content */
+                //foreach (string dir in dirs)
+                //{
+                //    MessageBox.Show(dir);
+                //}
+                //foreach (string file in files)
+                //{
+                //    MessageBox.Show(file);
+                //}
+            } else
+            {
+                MessageBox.Show("You haven't pick any folder yet");
+                label5.Text = "No File Chosen";
+                label5.ForeColor = System.Drawing.Color.Black;
+            }
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked && radioButton1.Checked)
+            {
+                // Do BFS and find all occurences
+            }
+            else if (checkBox1.Checked && radioButton2.Checked)
+            {
+                // Do DFS and find all occurences
+            }
+            else if(!checkBox1.Checked && radioButton1.Checked)
+            {
+                // Do BFS and find the first occurence only 
+            }
+            else if(!checkBox1.Checked && radioButton2.Checked)
+            {
+                // Do BFS and find the first occurence only
+            }
+            else
+            {
+                MessageBox.Show("Please choose any search algorithm option", "ErrorMessage");
+            }
+
+            /* Currently failed switch case, find out more soon */
+            //string a = "default";
+            //(checkBox1.Checked, radioButton1.Checked) switch
+            //{
+            //    (true, true) => string a = "aa",
+            //    (true, false) => string a = "ab",
+            //    (false, true) => string a = "ac",
+            //    (false, false) => string a = "ad",
+            //};
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
